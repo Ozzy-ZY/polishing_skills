939d132daf65ead3a81d92221d64e97b0fe85dcf
ozzy <zyadhimself1@gmail.com>
2024-03-18 23:15:12 +0200

    refactoring the code

--- polishing_skills/Encryption.cs
+++ polishing_skills/Encryption.cs
@@ -1,60 +1,60 @@
-﻿using System.Text;
+﻿using System;
+using System.Text;
 
 namespace polishing_skills
 {
     internal class Encryption
     {
-        private static string key = "";
+        private static StringBuilder key = new StringBuilder();
+        private static Random random = new Random();
+        private const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz" +
+                                     "0123456789`~!@#$%^&*()-_=+{}[];:'',./|";
+
         public static void keyInit(int length)
         {
-            Random random = new Random();
-            const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
             for (int i = 0; i < length; i++)
             {
-                key += chars[random.Next(chars.Length)];
+                key.Append(chars[random.Next(chars.Length)]);
             }
         }
         private static string roundKeyInit(int roundNumber)
         {
             char ch = (char)('a' + roundNumber);
-            string roundKey = "";
-            roundKey += ch;
-            roundKey += ch;
-            roundKey += ch;
-            roundKey += ch;
-            return xorStrings(roundKey, key);
+            string roundKey = new string(ch, 4);
+            return xorStrings(roundKey, key.ToString());
         }
+
         private static string xorStrings(string a, string b)
         {
-            if(a.Length != b.Length)
+            if (a.Length != b.Length)
             {
                 throw new ArgumentException("Strings must be of equal length");
             }
-            string result = "";
+            StringBuilder result = new StringBuilder(a.Length);
             for (int i = 0; i < a.Length; i++)
             {
-                result += (char)(((a[i] ^ b[i])%95)+33);
+                result.Append((char)(((a[i] ^ b[i]) % 95) + 33));
             }
-            return result;
+            return result.ToString();
         }
-        // the block must be 64 bits long
+
         public static string encrypt(string block, int numOfRounds)
         {
-            if(block.Length != 8)
+            if (block.Length != 8)
             {
                 throw new ArgumentException("Block must be 64 bits long");
             }
-            if (key == "")
+            if (key.Length == 0)
             {
                 keyInit(4);
             }
             string left = block.Substring(0, 4);
-            string right = block[4..];
+            string right = block.Substring(4, 4);
             for (int i = 1; i <= numOfRounds; i++)
             {
                 string temp = right;
                 string roundkey = roundKeyInit(i);
-                right = xorStrings(xorStrings(right,roundkey),left);
+                right = xorStrings(xorStrings(right, roundkey), left);
                 left = temp;
             }
             return right + left;
